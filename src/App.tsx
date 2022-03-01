c96cba708aa662caadb6046d8dc3629de2a76699
geunheejung <geunhee0212@gmail.com>
2022-03-01 15:12:22 +0900

    useTitle 구현.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
-import useTabs, { ContentsType } from './hooks/useTabs';
+import useTitle from "./hooks/useTitle";
 
 const App = () => {
-  const content: ContentsType = [
-    {
-      tab: 'Section 1',
-      content: `I'm the content of the Section 1`
-    },
-    {
-      tab: 'Section 2',
-      content: `I'm the content of the Section 2`
-    }
-  ]
+  const titleUpdater = useTitle('Home');
 
-  const tabs = useTabs(0, content);
-
-  const changeTab = (index: number) => {
-    tabs.changeCurrentIndex(index);
+  const moveTo = (pageName: string) => {
+    setTimeout(() => titleUpdater(pageName), 3000);
   }
 
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button
-          onClick={() => changeTab(index)}
-        >
-          {section.tab}
-        </button>
-      ))}
-      <ol>
-        <h1>{tabs.currentItem.tab}</h1>
-        <p>{tabs.currentItem.content}</p>
-      </ol>
+
+      <button onClick={() => moveTo('Login')}>Login</button>
     </div>
   );
 }
